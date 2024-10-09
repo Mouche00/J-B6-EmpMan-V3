e34cb740c9ab53ff0d259bf8c174cf4379964b81
Mushi <144794503+Mouche00@users.noreply.github.com>
2024-10-09 15:08:51 +0100

    JBE-23 // Add the Leave DAO extending GenericDAO

--- src/main/java/daos/implementations/LeaveDAOImpl.java
+++ src/main/java/daos/implementations/LeaveDAOImpl.java
@@ -4,7 +4,7 @@ import daos.interfaces.LeaveDAO;
 import models.Leave;
 
 public class LeaveDAOImpl extends GenericDAOImpl<Leave> implements LeaveDAO {
-//    protected LeaveDAOImpl(Class<Leave> entityClass) {
-//        super(entityClass);
-//    }
+    protected LeaveDAOImpl(Class<Leave> entityClass) {
+        super(entityClass);
+    }
 }
