c05b8d5ab47d4f77273de3d7baec8792320b05f2
Mushi <144794503+Mouche00@users.noreply.github.com>
2024-10-11 15:36:14 +0100

    JBE-45 // create application/applicant entities

---
+++ src/main/java/models/Application.java
@@ -0,0 +1,63 @@
+package models;
+
+import models.enums.ApplicationStatus;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "applications")
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "UUID")
+    private UUID id;
+
+    private String CV;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus status = ApplicationStatus.RECEIVED;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "applicant_id")
+    private BaseUser applicant;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCV() {
+        return CV;
+    }
+
+    public void setCV(String CV) {
+        this.CV = CV;
+    }
+
+    public ApplicationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ApplicationStatus status) {
+        this.status = status;
+    }
+
+    public BaseUser getApplicant() {
+        return applicant;
+    }
+
+    public void setApplicant(BaseUser applicant) {
+        this.applicant = applicant;
+    }
+
+    public Application(String CV) {
+        this.CV = CV;
+    }
+
+    public Application() {
+    }
+}
